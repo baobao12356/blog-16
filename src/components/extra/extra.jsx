719701b5bd406bad14ad602c874ec11d06c81772
Ada <wb-xyl390954@alibaba-inc.com>
2019-01-28 17:32:26 +0800

    gallery-done

--- src/components/extra/extra.jsx
+++ src/components/extra/extra.jsx
@@ -17,53 +17,68 @@ const Extra = props => {
     const handleShow = e => {
         const { target } = e.nativeEvent
         if (target.tagName.toUpperCase() === 'IMG') {
-            const CONTAINER_WIDTH = 900
-            const CONTAINER_HEIGHT = 600
-            const CONTENT_TOP = 120
-            const left = e.clientX - CONTAINER_WIDTH / 2
-            const clienX = e.clientX
-            const top = e.clientY - CONTAINER_HEIGHT / 2 - CONTENT_TOP
-            const el = document.getElementsByClassName(s.imgCon)[0]
-            const bigImg = el.children[0]
-            let timmer1 = null
-            let timmer2 = null
-            bigImg.addEventListener('click', e => { //开始缩小 
-                const handleAsync = () => {
-                    return new Promise(resolve => {
-                        el.style.cssText = `transform: translate(${left}px, ${top}px) scale(0.3); opacity: 0.5;`
-                        clearTimeout(timmer1)
-                        timmer1 = setTimeout(() => {
-                            resolve()
-                        }, 400);
-                    })
-                }
-                handleAsync().then(() => {
-                    bigImg.parentNode.parentNode.style.display = 'none'
-                    target.style.cssText = 'opacity: 0.5;transition: all 1s;'
-                })
+            const mask = document.getElementsByClassName(s.bigImg)[0]
+            const IMG_WIDTH = target.offsetWidth
+            const IMG_HEIGHT = target.offsetHeight
+            target.style.cssText = `width:640px; height: 420px;opacity: 1; z-index:1;translateX(0)`
+            mask.style.display = 'block'
+            mask.addEventListener('click', _ => {
+                target.style.cssText = `width:${IMG_WIDTH - 10}px; height: ${IMG_HEIGHT - 10}px;opacity: 0.5; z-index:0;`
+                mask.style.display = 'none'
             }, false)
-            const handleAsyncImg = () => {
-                return new Promise(resolve => {
-                    bigImg.src = target.src
-                    el.parentNode.style.display = 'block'
-                    el.style.cssText = `transition: all 0s; transform: translate(${left}px, ${top}px) scale(0.3)`
-                    target.style.cssText = 'opacity: 0;transition: all 0s;'
-                    clearTimeout(timmer2)
-                    timmer2 = setTimeout(() => {
-                        resolve()
-                    }, 10);
-                })
-            }
-            handleAsyncImg().then(() => {
-                el.style.cssText = `transform: translate(${clienX / 2}px, ${CONTAINER_HEIGHT / 2 + CONTENT_TOP - 380}px) scale(1); opacity:1; transition: all 0.5s;`
-            })
+            // return
+            // const IMG_WIDTH = target.offsetWidth
+            // const IMG_HEIGHT = target.offsetHeight
+            // const CONTAINER_WIDTH = 900
+            // const CONTAINER_HEIGHT = 600
+            // const CONTENT_TOP = 120
+            // const left = e.clientX - CONTAINER_WIDTH / 2
+            // const clienX = e.clientX
+            // const top = e.clientY - CONTAINER_HEIGHT / 2 - CONTENT_TOP
+            // const el = document.getElementsByClassName(s.imgCon)[0]
+            // const bigImg = el.children[0]
+            // let timmer1 = null
+            // let timmer2 = null
+            // const handleAsyncImg = () => {
+            //     clearTimeout(timmer2)                
+            //     return new Promise(resolve => {
+            //         bigImg.src = target.src
+            //         bigImg.style.cssText = `width: ${IMG_WIDTH}px;height:${IMG_HEIGHT}px`
+            //         el.parentNode.style.display = 'block'
+            //         el.style.cssText = `transition: all 0s; transform: translate(${left}px, ${top}px) scale(1)`
+            //         target.style.cssText = 'opacity: 0;transition: all 0s;'
+            //         timmer2 = setTimeout(() => {
+            //             resolve()
+            //         }, 10);
+            //     })
+            // }
+            // handleAsyncImg().then(() => {
+            //     bigImg.style.cssText = `width: 640px;height: 386px;`                
+            //     el.style.cssText = `transform: translate(${clienX / 2}px, ${CONTAINER_HEIGHT / 2 + CONTENT_TOP - 380}px) scale(1); opacity:1; transition: all 0.5s;`
+            // })
+            // bigImg.addEventListener('click', e => { //开始缩小 
+            //     const handleAsync = () => {
+            //         clearTimeout(timmer1)                    
+            //         return new Promise(resolve => {
+            //             bigImg.style.cssText = `width: ${IMG_WIDTH - 10}px;height:${IMG_HEIGHT - 10}px`   //10 ：padding                     
+            //             el.style.cssText = `transform: translate(${left}px, ${top}px) scale(1); opacity: 0;`
+            //             timmer1 = setTimeout(() => {
+            //                 resolve()
+            //             }, 400);
+            //         })
+            //     }
+            //     handleAsync().then(() => {
+            //         bigImg.parentNode.parentNode.style.display = 'none'
+            //         target.style.cssText = `opacity: 0.5;transition: all 1s;`
+            //     })
+            // }, false)
         }
     }
     return (
         <div className={s.extraContainer}>
             <div className={s.bigImg}>
                 <div id='imgCon' className={s.imgCon}>
-                    <img src="" alt="" />
+                    {/* <img src="" alt="" /> */}
                 </div>
             </div>
             <div className={s.galleryContainer} onClick={(e) => handleShow(e)}>
